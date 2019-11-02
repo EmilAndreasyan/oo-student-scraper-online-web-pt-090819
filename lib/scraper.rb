ee48690f44c13c4301ad4e8f18f2e3d777c1928c
Emil Andreasyan <emil.andreasyan@yahoo.com>
2019-11-02 06:08:23 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,11 +4,40 @@ require 'pry'
 class Scraper
 
   def self.scrape_index_page(index_url)
+    index_url = "https://learn-co-curriculum.github.io/student-scraper-test-page/index.html"
+    doc = Nokogiri::HTML(open(index_url))
+    name = doc.css("h4.student-name").text
+    location = doc.css("p.student-location").text
     
+    profile_url = doc.css(".view-profile-div")
+    student_hash = {
+      :name => name,
+      :location => location,
+      :profile_url => profile_url
+    }
+    Student.new(student_hash)
   end
 
   def self.scrape_profile_page(profile_url)
+    profile_url = "https://learn-co-curriculum.github.io/student-scraper-test-page/students/"
+    doc = Nokogiri::HTML(open(profile_url))
     
+    twitter = doc.css("https://twitter.com")
+    linkedin = doc.css("")
+    github = doc.css("")
+    blog = doc.css("")
+    profile_quote = doc.css("")
+    bio = doc.css("")
+    profile_hash = {
+      :twitter => twitter,
+      :linkedin => linkedin,
+      :github => github,
+      :blog => blog,
+      :profile_quote => profile_quote,
+      :bio => bio
+    }
+    Social.new(profile_hash)
+    binding.pry
   end
 
 end
